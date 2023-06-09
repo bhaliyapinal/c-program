7bd673c09e4eb106840d08e256f09afc1a9b13a9
bhaliyapinal <135968694+bhaliyapinal@users.noreply.github.com>
2023-06-09 13:36:50 +0530

    Add files via upload

---
+++ ABC.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+int myNum =15;
+float myfloatNum = 5.999;
+char myletter ='D';
+
+printf ("%d\n",myNum) ;
+printf("%f\n",myfloatNum);
+printf("%c\n",myletter);
+return 0;
+
+
+
+}
